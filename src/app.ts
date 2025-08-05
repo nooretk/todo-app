6170b79000858a2cec5b728f123389631f5932ba
Noureddin Etkaidek <noor.etco@gmail.com>
2025-08-05 14:18:34 +0300

    first commit

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+
+
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
