ae55edab8203194eb8c0e0f6d2fa034f1db83ea4
Noureddin Etkaidek <noor.etco@gmail.com>
2025-08-07 12:40:40 +0300

    Add LICENSE, README, Swagger documentation, and update package.json

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,14 @@
 import express from "express";
 import apiRouter from "./routes/api.routes";
+import swaggerUi from "swagger-ui-express";
+import swaggerDocument from "./swagger.json";
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
 app.use("/api", apiRouter);
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.get("/", (req, res) => res.status(200).send("Welcome to Todo API"));
 
